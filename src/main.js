c2c7d41c29dff4b6b27a6d5c9a96720564aef965
nanshou <1007274033@qq.com>
2020-03-19 01:03:09 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import fastClick from 'fastclick'
+import './assets/style/reset.css'
+import './assets/style/border.css'
+/* 该css样式用于解决移动端1像素边框问题。问题分析：有些手机的屏幕分辨率较高，是2-3倍屏幕。css样式中border:1px solid red;在2倍屏下，显示的并不是1个物理像素，而是2个物理像素。为了解决这个问题，引入border.css是非常有必要的 */
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
