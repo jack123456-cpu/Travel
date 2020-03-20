016e22a73ac43997c593aa00aa0254a93fe7da57
nanshou <1007274033@qq.com>
2020-03-20 12:31:38 +0800

    add search to city

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="search">
+    <input type="text" placeholder="输入城市名称或拼音" class="search-input" />
+  </div>
+</template>
+<script>
+export default {
+  name: "CitySearch"
+};
+</script>
+<style lang="stylus" scoped>
+@import '~style/varibles.styl';
+@import '~style/ellipsis.styl';
+
+.search {
+  height: 0.72rem;
+  padding: 0 0.1rem;
+  background: $bgColor;
+
+  .search-input {
+    box-sizing: border-box;
+    width: 100%;
+    height: 0.62rem;
+    padding: 0 0.1rem;
+    line-height: 0.62rem;
+    text-align: center;
+    border-radius: 0.06rem;
+    color: #666;
+  }
+}
+</style>
