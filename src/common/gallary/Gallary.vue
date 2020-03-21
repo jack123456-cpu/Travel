e9291f5d8f15b2d51e865f7370281503271555bd
nanshou <1007274033@qq.com>
2020-03-21 13:07:06 +0800

    gallary finshed

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container" @click="hiddenBanner">
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+        <!-- slides -->
+        <swiper-slide v-for="(item,index) in imgs" :key="index">
+          <img class="swiper-img" :src="item" alt="12" />
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Gallary",
+  props: {
+    imgs: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+        loop: true,
+        paginationType: "fraction",
+        observeParents: true,
+        observer: true
+      }
+    };
+  },
+  methods: {
+    hiddenBanner() {
+      this.$emit("hidden");
+    }
+  }
+};
+</script>
+<style lang="stylus" scoped>
+.container >>> .swiper-container {
+  overflow: visible;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  z-index: 99;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background-color: #000;
+
+  .wrapper {
+    background-color: #fff;
+    height: 0;
+    width: 100%;
+    padding-bottom: 100%;
+
+    .swiper-img {
+      width: 100%;
+    }
+
+    .swiper-pagination {
+      color: #FFF;
+      bottom: -1rem;
+      z-index: 999;
+    }
+  }
+}
+</style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="@/assets/images/123.jpg" alt="picture" />
+      <div class="banner-info">
+        <div class="banner-title">杭州猫猫世界(AAAA景区)</div>
+        <div class="banner-number">39</div>
+      </div>
+    </div>
+    <common-gallary :imgs="imgs" v-show="showGallary" @hidden="handleBannerHidden"></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from "common/gallary/Gallary";
+export default {
+  name: "Banner",
+  components: {
+    CommonGallary
+  },
+  data() {
+    return {
+      imgs: [
+        "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=31118306,972739979&fm=26&gp=0.jpg",
+        "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=31118306,972739979&fm=26&gp=0.jpg"
+      ],
+      showGallary: false
+    };
+  },
+  methods: {
+    handleBannerClick() {
+      this.showGallary = true;
+    },
+    handleBannerHidden() {
+      this.showGallary = false;
+    }
+  }
+};
+</script>
+<style lang="stylus" scoped>
+.banner {
+  position: relative;
+  overflow: hidden;
+  padding-bottom: 55%;
+  height: 0;
+
+  .banner-img {
+    width: 100%;
+  }
+
+  .banner-info {
+    display: flex;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    color: #fff;
+    line-height: 0.6rem;
+    background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8));
+  }
+
+  .banner-title {
+    font-size: 0.32rem;
+    padding: 0 0.2rem;
+    flex: 1;
+    vertical-align: middle;
+  }
+
+  .banner-number {
+    padding: 0 0.4rem;
+    margin-top: 0.14rem;
+    height: 0.32rem;
+    line-height: 0.32rem;
+    border-radius: 0.2rem;
+    background: rgba(0, 0, 0, 0.8);
+    font-size: 0.24rem;
+    color: #fff;
+  }
+}
+</style>
